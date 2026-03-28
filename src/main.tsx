45094a239ec032767477acf534605222c22a2f3b
roz-logic <desaurora722@gmail.com>
2026-03-28 15:32:55 +0300

    feat: add results app as static page

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter, Routes, Route } from "react-router-dom";
 import { App } from "./App";
-import { App as ResultsApp } from "./results/App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/results" element={<ResultsApp />} />
-      </Routes>
-    </HashRouter>
+    <App />
   </StrictMode>
 );
