05f5bc7dd07eeeb8e0b665568fd01e6a4f316f9e
roz-logic <desaurora722@gmail.com>
2026-03-28 15:18:37 +0300

    fix: switch to HashRouter for GitHub Pages compatibility

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { App } from "./App";
 import { App as ResultsApp } from "./results/App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/temporal-binding-predictive-processing">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/results" element={<ResultsApp />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
