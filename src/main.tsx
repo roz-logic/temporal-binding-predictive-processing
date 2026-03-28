a7c05e781d377fa69522ad49a094403d50e79949
roz-logic <desaurora722@gmail.com>
2026-03-28 15:15:11 +0300

    fix: correct results route path

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { App } from "./App";
 import { App as ResultsApp } from "./results/App";
 import "./index.css";
@@ -10,9 +10,7 @@ createRoot(document.getElementById("root")!).render(
     <BrowserRouter basename="/temporal-binding-predictive-processing">
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="results" element={<ResultsApp />} />
-        <Route path="results/*" element={<ResultsApp />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="/results" element={<ResultsApp />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
